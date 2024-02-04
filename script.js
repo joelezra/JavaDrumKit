03b891260de9e71729533778d89af9f644cec69c
Joel Joe <joelezra10@gmail.com>
2024-02-04 19:20:15 +0000

    added playSound function in js

---
+++ script.js
@@ -0,0 +1,11 @@
+// function that plays the corresponding audio to keys pressed on keyboard, ignoring unassigned keys
+
+function playSound (event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing'); // adds 'playing' class to keys pressed to style the transition
+    audio.currentTime = 0; // makes audio play from start every time key is pressed
+    audio.play();
+}
