f103744c455715b568c8a6b09ed7f2e338c223d7
Joel Joe <joelezra10@gmail.com>
2024-02-04 19:24:20 +0000

    added eventlistener for keydown event

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ function playSound (event) {
     audio.currentTime = 0; // makes audio play from start every time key is pressed
     audio.play();
 }
+
+const keys = Array.from(document.getElementsByClassName('keys')); 
+window.addEventListener('keydown', playSound); // listens for keydown event to execute playSound function
